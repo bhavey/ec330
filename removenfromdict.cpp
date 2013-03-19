ff75e5db1a8dd9dde2cc7148d70b87054933a386
tagsby <benhavey@gmail.com>
2013-03-19 15:58:55 -0400

    Condensed the functionality of the dictionary parsing functions into one function.

--- removenfromdict.cpp
+++ removenfromdict.cpp
@@ -7,6 +7,7 @@ using namespace std;
 #include <vector>
 #include <string>
 #include <regex.h>
+//#include <algorithm>
 
 int main() {
     ifstream readf ("dictionary");
@@ -17,15 +18,19 @@ int main() {
     char cur_str[25];
     char prev_str[25];
     while (readf >> cur_str) {
+        for (int i=0; i<(sizeof(cur_str)/sizeof(char)); i++)
+            cur_str[i]=tolower(cur_str[i]);
         regcomp(&re, "n$", REG_EXTENDED);
         if (regexec(&re, cur_str, 1, &match, 0) == 0) {
             //Ends with an n. My name is Ben. This is no good.
-            printf("%s contains an n.\n",cur_str);
+            printf("%s ends a n.\n",cur_str);
         } else {
             //This time print the string
-            fprintf(ndict, "%s\n", cur_str);
+            if (strcmp(cur_str,prev_str)!=0)
+                fprintf(ndict, "%s", cur_str);
         }
-        prev_str=cur_str
+        //Check this versus the previous string.
+        strcpy(prev_str,cur_str);
     }
     fclose(ndict);
     readf.close();
