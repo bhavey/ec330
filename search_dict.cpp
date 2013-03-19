8b2f02aabd56dad2f5a84a04d200d9f62bddd5e1
tagsby <benhavey@gmail.com>
2013-03-19 16:52:37 -0400

    Added search_dict

---
+++ search_dict.cpp
@@ -0,0 +1,13 @@
+using namespace std;
+#include <iostream>
+#include <fstream>
+#include <stdio.h>
+#include <string>
+#include <regex.h>
+#include <pthread.h>
+
+int main() {
+    int n, flagvar;
+    pthread_t *threads;
+    pthread_attr_t attr;
+}
