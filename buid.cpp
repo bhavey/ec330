0bb935923cc65a80c0f70678fd0eedbd4a605a44
tagsby <benhavey@gmail.com>
2013-03-18 22:00:53 -0400

    Added forking to buid

--- buid.cpp
+++ buid.cpp
@@ -19,26 +19,42 @@ int main() {
 
     if( !in )
         return 0;
-    int numb=0;
+    int numb=1;
 
-    long long pos=0;
-//    for 
-    while (in >> word) {
-        numb++;
-        if (numb%10000==0)
-            printf("numb: %d\n",numb);
-        if (numb%100000==0) {
-            pos = in.tellg();
-            in.close();
+    long long pos=8063854;
+
+    pid_t pid;
+    for (int i = 0; i<100; i++) {
+
+        if ((pid = fork()) == 0) { //child process
             fstream in ("BigData.txt");
-            in.seekg(pos);
+            in.ignore(pos, '\0');
             printf("pos: %lld\n",pos);
-        }
-        p=word.c_str();
-        regcomp(&re, "U[0-9][0-9][0-9][0-9][0-9][0-9][0-9][0-9]", REG_EXTENDED);
-        while(regexec(&re, p, 1, &match, 0) == 0) {
+
+            while (in >> word) {
+                numb++;
+                if (numb%10000==0) {
+                    printf("\n\n");
+                    printf("pos: %lld\n",pos);
+                    printf("numb: %d\n",numb);
+                }
+                if (numb%100000==0) {
+                    pos = in.tellg();
+                    in.close();
+                    printf("pos: %lld\n",pos);
+                    break;
+                }
+                p=word.c_str();
+                regcomp(&re, "U[0-9][0-9][0-9][0-9][0-9][0-9][0-9][0-9]", REG_EXTENDED);
+                while(regexec(&re, p, 1, &match, 0) == 0) {
 //            printf("%.*s\n", (int)(match.rm_eo - match.rm_so), &p[match.rm_so]);
-            p += match.rm_eo;
+                    p += match.rm_eo;
+                }
+            }
+        } else { //parent process.
+            pid_t childPid;
+            int status;
+            childPid = wait(&status); //wait for the child to finish.
         }
     }
 
