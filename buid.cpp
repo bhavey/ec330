3b72a09e8af99bf5c183d8f6480245204fe5a657
tagsby <benhavey@gmail.com>
2013-03-18 21:14:22 -0400

    closer with buid

--- buid.cpp
+++ buid.cpp
@@ -1,9 +1,11 @@
 using namespace std;
 #include <iostream>
+#include <fstream>
 #include <math.h>
 #include <regex.h>
 #include <stdio.h>
 #include <limits>
+#include <vector>
 
 int main() {
     regex_t re;
@@ -11,14 +13,33 @@ int main() {
     const char *p = file;
     regmatch_t match;
 
-    if(regcomp(&re, "[[:space:]]U[0-9][0-9][0-9][0-9][0-9][0-9][0-9][0-9][[:space:]]", REG_EXTENDED) != 0) {
-        printf("No results found.\n");
-        return -1;
-    }
+    fstream in ("BigData.txt");
+    vector<string> words;
+    string word;
 
-    while(regexec(&re, p, 1, &match, 0) == 0) {
-        printf("%.*s\n", (int)(match.rm_eo - match.rm_so), &p[match.rm_so]);
-        p += match.rm_eo;
+    if( !in )
+        return 0;
+    int numb=0;
+
+    long long pos=0;
+//    for 
+    while (in >> word) {
+        numb++;
+        if (numb%10000==0)
+            printf("numb: %d\n",numb);
+        if (numb%100000==0) {
+            pos = in.tellg();
+            in.close();
+            fstream in ("BigData.txt");
+            in.seekg(pos);
+            printf("pos: %lld\n",pos);
+        }
+        p=word.c_str();
+        regcomp(&re, "U[0-9][0-9][0-9][0-9][0-9][0-9][0-9][0-9]", REG_EXTENDED);
+        while(regexec(&re, p, 1, &match, 0) == 0) {
+//            printf("%.*s\n", (int)(match.rm_eo - match.rm_so), &p[match.rm_so]);
+            p += match.rm_eo;
+        }
     }
 
     return 0;
