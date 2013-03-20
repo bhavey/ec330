204c7dcc8522fc032ec313c33d18eab31400b991
tagsby <benhavey@gmail.com>
2013-03-19 21:28:16 -0400

    Now works in 3 days!

--- newData.cpp
+++ newData.cpp
@@ -9,25 +9,43 @@ using namespace std;
 #include <algorithm>
 
 int main() {
+    regex_t re;
+    regmatch_t match;
     FILE *writeover;
     writeover = fopen("NewData.txt","w");
     fclose(writeover);
     char * pch;
+    const char *p1;
+    const char *p2;
     long int numb;
     string word;
+    string dict_word;
     fstream dict ("odict");
     fstream in ("BigData.txt");
-    fstream out ("newData.txt");
+    fstream out ("NewData.txt");
     while (in >> word) {
-        dict.seekg(0); //Bring dict to the beginning!
         //cut out the empty strings!
         pch=strtok((char*)word.c_str(),"0123456789");
         while (pch != NULL) {
+            dict.seekg(0); //Bring the dictionary to the beginning!
             numb++;
-            if (numb%500000==0)
+            if (numb%10==0)
                 printf("At word: %ld\n",numb);
 //            out << pch;
 //            out << "\n";
+            while (dict >> dict_word) {
+                p1=word.c_str();
+                p2=dict_word.c_str();
+                if (strlen(p2) > strlen(p1) ) {
+                    break;
+                }
+                regcomp(&re, p2, REG_ICASE);
+                while (regexec(&re, p1, 1, &match, 0) == 0) {
+        //                    printf("%s contains %s\n",p1,p2);
+                    p1 += match.rm_eo;
+                }
+                regfree(&re);
+            }
             pch = strtok(NULL, "0123456789");
         }
     }
