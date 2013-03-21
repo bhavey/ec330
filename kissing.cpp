8a6b65ceab2fdf984e89bcf72a30ee02b0442900
tagsby <benhavey@gmail.com>
2013-03-21 02:51:43 -0400

    All Im doing tonight. Stuff started changing hardcore when I started culling out the single letter palindromes

--- kissing.cpp
+++ kissing.cpp
@@ -7,6 +7,9 @@
 #include <map>
 
 using namespace std;
+bool fullalph(map<char,int>);
+void printpal(vector<string>);
+int test_in(string s, vector<string> pal);
 
 int main() { //Test program
     //Screw it. I don't even care. Just throw the whole damned dictionary into here.
@@ -23,12 +26,17 @@ int main() { //Test program
 
     char c='a'; //Start examining at a, loop through to z
     string entry;
-    while (pald >> entry) { //Put the palindrome entries into a vector
-        pal.push_back(entry);
-    }
+    while (pald >> entry) //Put the palindrome entries into a vector
+        if (entry.length()!=1)
+            pal.push_back(entry);
+
+    for (int i=0; i<pal.size(); i++)
+        printf("%s\n",pal.at(i).c_str());
+
 
     while (flp >> entry) //Put the flip entries into a vector
-        flip.push_back(entry);
+        if (entry.length()!=1)
+            flip.push_back(entry);
 
     string tmp1;
     string tmp2;
@@ -50,16 +58,74 @@ int main() { //Test program
 
 //    map<char,vector<string> > smap; //map of the starting chars.
 //    vector<string> kiss; //The kissing palindrome!
+//tmap is the individ ones
     char cur_lo;
     char start='a';
+    int newflag;
+    int jflag, kflag, lflag, mflag, nflag;
     for (int i=0; i<pal.size(); i++) { //form the actual kissing palindromes!
         start='a';
         for (int j=0; j<26; j++)
             amap[start+j]=0; //clear out the amap!
         for (int j=0; j<26; j++) {
-            for (int k=0; k<smap[start+j].size(); k++) {
-                smap[start+j].at(k)="poop";
-            }
+            newflag=0;
+            lflag=0;
+            kiss.clear(); //clean it every time we reach here!
+//            for (int k=0; k<26; k++) {
+                for (int l=0; l<(emap[start+j].size())&&!lflag; l++) {
+                    if (!newflag)
+                        kiss.push_back(emap[start+j].at(l)); //Put this piece on the top.
+                    else {
+                        tmp1=emap[start+j].at(l);
+                        test_in(tmp1, kiss);
+                    }
+                    for (int m=0; m<26; m++) //Add to the main maping
+                        amap[start+m]=amap[start+m]+tmap[emap[start+j].at(l)][start+m];
+                    for (int m=0; m<smap[emap[start+j].size()].size(); m++) {
+                        nflag=0;
+                        tmp1=smap[start+j].at(m);
+                        for (int n=0; n<kiss.size(); n++) { //make sure there are no repeats
+                            if (tmp1==kiss.at(n)) {         //in the palindrome.
+                                nflag=1;
+                                break;
+                            }
+                        }
+
+                        kiss.push_back(tmp1);
+                        for (int n=0; n<26; n++)
+                            amap[start+n]=amap[start+n]+tmap[emap[start+j].at(l)][start+n];
+                        if (fullalph(amap)) {
+                            printpal(kiss);
+                            lflag=1;
+                        }
+//                            printf("Found a palindrome!\n");
+                    }
+                }
+  //          }
         }
+        printf("i:%d\n",i);
     }
+}
+
+bool fullalph(map<char,int> inmap) {
+    char start='a';
+    for (int i=0; i<26; i++) {
+        if (inmap[start+i]==0)
+            return false;
+    }
+    return true;
+}
+
+void printpal(vector<string> pal) {
+    for (int i=0; i<pal.size(); i++)
+        printf("%s, ",pal.at(i).c_str());
+    printf("\n");
+}
+
+int test_in(string s, vector<string> pal) {
+    for (int i=0; i<pal.size(); i++) { //make sure there are no repeats
+        if (s==pal.at(i))           //in the palindrome.
+            return 1;
+    }
+    return 0;
 }
