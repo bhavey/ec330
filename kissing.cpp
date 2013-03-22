6dd8bbbd60d64f456b18577ad819567c62011122
tagsby <benhavey@gmail.com>
2013-03-21 22:36:18 -0400

    Final kissing program

--- kissing.cpp
+++ kissing.cpp
@@ -6,6 +6,7 @@
 #include <stdlib.h>
 #include <algorithm>
 #include <map>
+#include <time.h>
 
 using namespace std;
 bool fullalph(map<char,int>); //Sees if the map contains all the alphabetic characters
@@ -22,6 +23,7 @@ bool check_end(string s, map<char,vector<string> > &map1); //check if s is the l
 int sum_map(map<char,int> &in);
 
 int main() { //Test program
+    srand (time(NULL));
     //Screw it. I don't even care. Just throw the whole damned dictionary into here.
     vector<string> pal; //Contains only the relevant palindromes
     vector<string> kiss; //The kissing palindrome!
@@ -49,45 +51,85 @@ int main() { //Test program
     }
     zero_map(all_maps);
 
-//bool test_vector(vector<string> pal, string s); //tests if a string is inside of a vector
-//bool skipqxz(char c); //skips the character if it's q, x, or z
-//bool maps_diff(map<char,int> &map1, map<char,int> &map2);//True is map1/2 have overlaping char
-//map<char,vector<string> > start_map; //map of the starting chars.
-//map<string, map<char,int> > alph_map; //map containing a value for every individual pal.
-//map<char,int> all_maps; //map containing the value of the kissing pal.
     map<char,int> tmp_map; //map containing the value of the kissing pal.
     bool jflag;
     int length, final_length=99999;
     char last_char;
     string prev="a";
-    string cur="a";
-    string cur_final="a";
+    string cur="";
+    string cur_final="";
     int randval;
     int randpos;
     int itrs=1;
-//    int sum_map(map<char,int> &in) {
-//    bool fullalph(map<char,int> inmap) { //check if every letter is present in the map
-//void add_maps(map<char,int> &out, map<string,map<char,int> > &temp, string s) {
-    randval=rand()%26;
-    randpos=rand()%(start_map['a'+randval].size());
-    cur_final=start_map['a'+randval].at(randpos);
-    printf("first: %s\n",cur_final.c_str());
-    last_char=*(cur_final.end()-1);
-    for (i=0; i<10; i++) {
-        zero_map(tmp_map);
-        add_maps(all_maps,alph_map,cur_final);
-        randpos=rand()%(start_map['a'+randval].size());
-        prev=cur_final; //put the last one on this one!
-        kiss.push_back(prev); //throw that on kiss!
-        last_char=*(cur_final.end()-1);
 
+string newstr="duggolferreknittwasspeccammhoovaajarrobeeyeeborrajaavoohmmaccepssawttinkerrefloggud";
+    map<char,int> poopmap;
+    printf("string length: %d\n",(int)newstr.size());
+    for (int j=0; j<newstr.size(); j++)
+        for (int i=0; i<26; i++) //Adds together out+temp[s], stores in out.
+            if (*(newstr.begin()+j)=='a'+i)
+                poopmap['a'+i]=1;
+    printmap(poopmap,0);
+
+    int cur_sum;
+    int fin_sum=0;
+
+    for(int k=0; k<10; k++) {
+        randval=rand()%26;
+        randpos=rand()%(start_map['a'+randval].size());
+        cur_final=start_map['a'+randval].at(randpos);
+        printf("first: %s\n",cur_final.c_str());
+        last_char=*(cur_final.end()-1);
+        int size_range;
+        for (i=0; i<20; i++) {
+            zero_map(tmp_map);
+            if (strcmp(cur_final.c_str(),prev.c_str())==0) {
+                    randval=rand()%26;
+                    randpos=rand()%(start_map['a'+randval].size());
+                    prev=start_map['a'+randval].at(randpos);
+                kiss.pop_back();
+                prev=kiss.at(kiss.size()-1);
+            } else {
+                add_maps(all_maps,alph_map,cur_final);
+                prev=cur_final; //put the last one on this one!
+                kiss.push_back(prev); //throw that on kiss!
+            }
+            last_char=*(prev.end()-1);
+            for (int j=0; j<1000; j++) {
+                if (start_map[last_char].size()==0) { //nothing here! go backkkk.
+                    cur_final=prev;
+                    break;
+                }
+                tmp_map=all_maps;
+                randpos=rand()%(start_map[last_char].size());
+                cur=start_map[last_char].at(randpos);
+                add_maps(tmp_map,alph_map,cur);
+                cur_sum=sum_map(tmp_map); //fuck this shit. Those letters are rare.
+                if (*(cur.end()-1)=='y'||*(cur.end()-1)=='u'||*(cur.end()-1)=='w'
+                ||*(cur.end()-1)=='q'||*(cur.end()-1)=='x'||*(cur.end()-1)=='z') {
+                    continue;
+                }
+                if (cur_sum>fin_sum) {
+                    fin_sum=cur_sum;
+                    cur_final=cur;
+                }
+            }
+        }
+        printpal(kiss);
+        fin_sum=0;
+        cur_sum=0;
+        kiss.clear();
+    }
     return 0;
 }
 
 int sum_map(map<char,int> &in) {
-    int out;
+    int out=0;
     for (int i=0; i<26; i++)
-        out+=in['a'+i];
+        if (skipqxz('a'+i))
+            continue;
+        else if (in['a'+i]==1)
+            out+=1;
     return out;
 }
 
