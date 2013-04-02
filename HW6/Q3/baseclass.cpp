ba42ed249013aee8305e097d5b897c3142c4b65d
tagsby <benhavey@gmail.com>
2013-04-02 18:19:25 -0400

    wtf

--- HW6/Q3/baseclass.cpp
+++ HW6/Q3/baseclass.cpp
@@ -5,35 +5,47 @@
 #include <iterator>
 #include <iostream>
 #include <vector>
+#include <cstdlib>
 
 using namespace std;
 //Creates a Base.
 class Base {
 public:
     Base () {};
-    Base (int* a,int b);
+    Base (int *a, int b, int s);
     Base operator ++(int);
     void setBase(int);
-    void setContents(int*);
+    void setContents(int*, int);
     void print();
-    void clearBase();
+    void clear();
     bool checkEmpty();
 
 private:
     int *contents;
     int base;
+    int size;
 };
 
-Base::Base (int *a, int b) {
-    contents=a;
+Base::Base (int *a, int b, int s) {
     base=b;
-    for (int i=0; i<sizeof(contents)/sizeof(int); i++)
-        if (contents[i]>=base)
-            contents[i]=0;
+    size=s;
+    int j;
+    int cont[size];
+    for (int i=0; i<size; i++)
+        cont[i]=a[i];
+    contents=cont;
+    for (int i=0; i<size; i++)
+            if (contents[i]>=base) {
+                j=i;
+//THIS HERE****
+//                printf("Too big at %d\n",i);
+//THE CODE DOESNT WORK WITHOUT THIS PRINT STATEMENT.
+                contents[i]=0;
+            }
 }
 
 Base Base::operator++(int a) {
-    for (int i=0; i<sizeof(contents)/sizeof(int); i++) {
+    for (int i=0; i<size; i++) {
         contents[i]++;
         if (contents[i]==base)
             contents[i]=0;
@@ -45,41 +57,49 @@ Base Base::operator++(int a) {
 void Base::setBase(int b) {
     base=b;
 }
-void Base::setContents(int *in) {
-    for (int i=0; i<sizeof(contents)/sizeof(int); i++) {
-        if (contents[i]>=base) {
-            printf("Please set contents to base %d\n",base);
-            return;
+void Base::setContents(int *in, int s) {
+    int cont[size];
+    for (int i=0; i<size; i++)
+        cont[i]=in[i];
+    for (int i=0; i<size; i++) {
+        if (cont[i]>=base) {
+            cont[i]=0;
         }
     }
-    contents=in;
+    contents=cont;
 }
 
 void Base::print() {
-    printf("base: %d\n",base);
-//    printf("print: %d\n",this->contents[0]);
-//    for (int i=0; i<sizeof(contents)/sizeof(int); i++)
-//        printf("%d%c",contents[i],(i==(sizeof(contents)/sizeof(int)-1)?'\n':','));
+    for (int i=0; i<size; i++)
+        printf(" %d%c",contents[i],(i==size-1?'\n':','));
     return;
 }
 
-void Base::clearBase() {
-    for (int i=0; i<sizeof(contents)/sizeof(int); i++)
+void Base::clear() {
+    for (int i=0; i<size; i++)
         contents[i]=0;
     return;
 }
 
 bool Base::checkEmpty() {
-    for (int i=0; i<sizeof(contents)/sizeof(int); i++)
+    for (int i=0; i<size; i++)
         if (contents[i] != 0)
             return false;
     return true;
 }
 
 int main() {
-    int colormap[4]={1,1,1,3};
-    Base b2 (colormap,3);
+    int colormap[10]={9,2,0,1,2,1,5,3,1,3};
+    Base b2 (colormap,3,10);
     b2.print();
-    b2.setContents(colormap);
+
+    b2.clear();
+    b2.print();
+    printf("Colormap: ");
+    for (int i=0; i<10; i++)
+        printf("%d, ",colormap[i]);
+    printf("\n");
+    b2.setContents(colormap,10);
+    b2.print();
     return 0;
 }
