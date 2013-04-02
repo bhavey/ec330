c70806e6987d13a912b23c820657cc6549a9d1d3
tagsby <benhavey@gmail.com>
2013-04-01 20:46:47 -0400

    Added functions to the graph

--- HW6/Q3/Graph.cpp
+++ HW6/Q3/Graph.cpp
@@ -3,6 +3,38 @@
 
 using namespace std;
 
+int Graph::addVertex(int color) { //Create a vertex of color. Return ID of the vertex
+    vertexIterator it = vertices.end(); //Go to the last item in the set;
+    vertices.insert(*(--it)+1); //Add the new vertex!
+    int vertID=*(++it); //Get the ID of the new vertex.
+    colors[vertID] = color; //Apply the color to the new ID.
+    return vertID; //return the new vertex ID
+}
+
+void Graph::addEdge(directedEdge newEdge) {
+    directedEdge tmpEdge = newEdge;
+    vertexIterator vend = vertices.end();
+    vertexIterator it1 = vertices.find(newEdge.first);
+    vertexIterator it2 = vertices.find(newEdge.second);
+    if ((it1==vend)||(it2==vend)) {
+        printf("One or both vertices isn't initialized.\n");
+        return;
+    } else {
+    
+    }
+    return;
+}
+
+int Graph::getColor(int vertex) {
+
+    return vertex;
+}
+
+bool Graph::isEdge(directedEdge newEdge) {
+
+    return true;
+}
+
 string Graph::print() {
 	stringstream result;
 	for (vertexIterator vert1=vertices.begin(); vert1 != vertices.end(); vert1++) {
@@ -13,4 +45,9 @@ string Graph::print() {
 		result << endl;
 	}
 		return result.str();
+}
+
+int main() {
+    printf("Poop!\n");
+    return 0;
 }
