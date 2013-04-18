82e67f2dedc6250dbc0e723685b697c2ae96a4b3
tagsby <benhavey@gmail.com>
2013-04-18 18:56:04 -0400

    Added simple streak checking

--- HW7/Q4/exPlayer.cpp
+++ HW7/Q4/exPlayer.cpp
@@ -1,12 +1,17 @@
 #include <iostream>
 #include <cstdlib>
+#include <string.h>
 #include "player.h"
 #include "exPlayer.h"
 #define size 10
 #define boardSize 10
 
+int edge=size-3;
+
 exPlayer::exPlayer(square **initTable) {
-  myColor=ex;
+    xstreak=-1;
+    ystreak=-1;
+    myColor=ex;
     table = new int*[size];
     for (int ii=0; ii<size; ii++) {
         table[ii]=new int[size];
@@ -22,15 +27,46 @@ void exPlayer::otherMove(boardSquare bs) {
 	table[bs.xx][bs.yy]=(myColor==ex?oh:ex);
 }
 
+bool CheckDir(char s[2], int x, int y) {
+    if (s=="N")
+        if (y>3)
+            return 1;
+    else if (s=="NE")
+        if ((y>3) && (x<edge))
+            return 1;
+    else if (s=="E")
+        if (x<edge)
+            return 1;
+    else if (s=="SE")
+        if ((y<edge) && (x<edge))
+            return 1;
+    else if (s=="S")
+        if (y<edge)
+            return 1;
+    else if (s=="SW")
+        if ((y<edge) && (x>3))
+            return 1;
+    else if (s=="W")
+        if (x>3)
+            return 1;
+    else if (s=="NW")
+        if ((y>3) && (x>3))
+            return 1;
+    return 0; //Nope!
+}
+
 boardSquare exPlayer::nextMove() {
     int x; //The two variables that we'll check for a location against.
     int y;
     int max_adj; //The amount of adjacent pieces
     boardSquare bs;
     while (table[bs.xx][bs.yy]!=blank) {
-        bs.xx = rand()%boardSize;
-        bs.yy = rand()%boardSize;
+        x = rand()%boardSize;
+        y = rand()%boardSize;
+        bs.xx = x;
+        bs.yy = y;
     }
+
     table[bs.xx][bs.yy]=ex;
     return bs;
 }
--- HW7/Q4/exPlayer.h
+++ HW7/Q4/exPlayer.h
@@ -1,20 +1,25 @@
+
 #ifndef exh
 #define exh
 #include "player.h"
+#include <string.h>
 
 class exPlayer:public player{
 public:
   exPlayer(square **initTable);     // initialize player
   boardSquare nextMove();        // computes a next move for the player and stores coordinates in (xx,yy)
   void otherMove(boardSquare bs);// records the move of the opponent
+  bool CheckDir(char s[2], int x, int y); //Checks the directionality on the board,
+  //see if can move 4 pieces in any given direction.
 
 private:
 	int **table; // maintain my own copy of the board
-	int xstreak[1][1]; //Keeps up a list of streak location. If there is a no current streak,
+	int xstreak; //Keeps up a list of streak location. If there is a no current streak,
+	int ystreak;
 	//values = -1,-1. Otherwise contains the xy start location.
 	int streak_length; //Currect steak length. At streak = 2, jump one to fool the opponent
 	//from a block.
-	string direction; //Contains possible directions. Choices are as follows:	
+	char direction[2]; //Contains possible directions. Choices are as follows:	
 	//N, NE, E, SE, S, SW, W, NW
 };
 #endif
