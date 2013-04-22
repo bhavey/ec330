e6c9f779d6dcc4bee988101c4c638cd601203be5
tagsby <benhavey@gmail.com>
2013-04-22 17:36:46 -0400

    Now accepts username and password from command line for simplicity

--- HW8/socket/simple_client_main.cpp
+++ HW8/socket/simple_client_main.cpp
@@ -6,19 +6,26 @@
 
 using namespace std;
 
-int main (int argc, int argv[]) {
-	try {
+int main (int argc, char* argv[]) {
+ 	if (argc!=3) {
+ 		printf("Incorrect format! Use ./PROG USER PASS\n")
+ 		return -1;
+ 	}
+ 	try {
 		//Algorithmics.bu.edu is 128.197.185.27
-		ClientSocket client_socket ("128.197.185.27", 8001);
+		ClientSocket client_socket("128.197.185.27", 8001);
 		string send;
 		string reply;
-      
+		char tmp[1000];
+
 		while (send!="done") {
 			cout << "CLIENT: ";
 			getline(cin,send); // i.e. get the whole line (this is important, cin >> send stops at a whitespace
-			client_socket << send+"\n";
-			sleep(1);   // give a little time between send and receive (1 second is usually overkill,
-						//but it's safe)
+			sprintf(tmp,"%s %s %s\n",argv[1],argv[2],send.c_str()); //Write the formatted string to tmp
+			send=tmp;
+
+			client_socket << send;
+			usleep(500000);   // give a little time between send and receive.
 			client_socket >> reply;
 			cout << "SERVER:  " << reply << endl;
 		}
