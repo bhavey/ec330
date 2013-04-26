e1a31b31650d37b21a29ecdff24020238e25676e
tagsby <benhavey@gmail.com>
2013-04-26 17:36:59 -0400

    shitty code, hoping to fix

--- HW8/socket/simple_client_main.cpp
+++ HW8/socket/simple_client_main.cpp
@@ -3,10 +3,14 @@
 #include <iostream>
 #include <string>
 #include <unistd.h>
-
+#include <time.h>
 using namespace std;
 
 int main (int argc, char* argv[]) {
+	string reply;
+	string tmpreply;
+	int recv;
+
  	if (argc!=3) {
  		printf("Incorrect format! Use ./PROG USER PASS\n");
  		return -1;
@@ -15,7 +19,7 @@ int main (int argc, char* argv[]) {
 		//Algorithmics.bu.edu is 128.197.185.27
 		ClientSocket client_socket("128.197.185.27", 8001);
 		string send;
-		string reply;
+		string old_reply="jjs";
 		char tmp[1000];
 
 		while (send!="done") {
@@ -23,15 +27,25 @@ int main (int argc, char* argv[]) {
 			getline(cin,send); // i.e. get the whole line (this is important, cin >> send stops at a whitespace
 			sprintf(tmp,"%s %s %s\n",argv[1],argv[2],send.c_str()); //Write the formatted string to tmp
 			send=tmp;
-
+			int allChars=-12;
 			client_socket << send;
-			usleep(1000000);   // give a little time between send and receive.
-			client_socket >> reply;
+
+			while (strcmp(reply.c_str(),old_reply.c_str())) {
+				cout << "Here!\n";
+				old_reply=reply;
+				usleep(10000);
+				client_socket >> tmpreply;
+				reply+=tmpreply;
+			}
+			cout << "This: " << reply << endl;
+			cout << "allChars: " << allChars << endl;
+			cout << "reply size: " << (int)reply.size() << endl;
 			cout << "SERVER:  " << reply << endl;
 		}
 	}
 	catch(SocketException& e) {
 		cout << "Exception was caught:" << e.description() << "\n";
+		cout << "Reply: " << reply << endl;
 	}
 	return 0;
 }
