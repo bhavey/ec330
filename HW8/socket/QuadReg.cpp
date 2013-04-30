f2841cb2dbb50bf9584646222c405f92bbbb9a51
tagsby <benhavey@gmail.com>
2013-04-30 18:00:56 -0400

    Works for small datasets but not large ones

--- HW8/socket/QuadReg.cpp
+++ HW8/socket/QuadReg.cpp
@@ -6,39 +6,45 @@ using namespace std;
 //Reimplemented from http://www.codeproject.com/Articles/63170/Least-Squares-Regression-for-Quadratic-Curve-Fitti
 //Code was a C# class, implemented by Alex Etchells. Changed to C++.
 //Uses s{jk} notation.
+//Incorporated different containers and implemented clear and find zero functions.
 
 class QuadReg {
 public:
     //constructor
     QuadReg() {
         numOfEntries = 0;
-        pair<float,float> pointpair;
+        pair<long double,long double> pointpair;
+    }
+
+    void ClearContents() {
+        numOfEntries = 0;
+        pointArray.clear();
     }
 
     //add point pairs
-    void AddPoints(float x, float y)  {
-        pair<float,float> pointpair (x,y); 
+    void AddPoints(long double x, long double y)  {
+        pair<long double, long double> pointpair (x,y); 
         numOfEntries +=1; 
         pointArray.push_back(pointpair);
     }
 
     //returns the a term of the equation ax^2 + bx + c
-    float aTerm() {
+    long double aTerm() {
         if (numOfEntries < 3) {
             printf("Insufficient pairs of co-ordinates\n");
             return 0;
         }
         //notation sjk to mean the sum of x_i^j*y_i^k. 
-        double s40 = getSx4(); //sum of x^4
-        double s30 = getSx3(); //sum of x^3
-        double s20 = getSx2(); //sum of x^2
-        double s10 = getSx();  //sum of x
-        double s00 = numOfEntries;
+        long double s40 = getSx4(); //sum of x^4
+        long double s30 = getSx3(); //sum of x^3
+        long double s20 = getSx2(); //sum of x^2
+        long double s10 = getSx();  //sum of x
+        long double s00 = numOfEntries;
         //sum of x^0 * y^0  ie 1 * number of entries
 
-        double s21 = getSx2y(); //sum of x^2*y
-        double s11 = getSxy();  //sum of x*y
-        double s01 = getSy();   //sum of y
+        long double s21 = getSx2y(); //sum of x^2*y
+        long double s11 = getSxy();  //sum of x*y
+        long double s01 = getSy();   //sum of y
 
         //a = Da/D
         return (s21*(s20 * s00 - s10 * s10) - 
@@ -51,22 +57,22 @@ public:
     }
 
     //returns the b term of the equation ax^2 + bx + c
-    float bTerm() {
+    long double bTerm() {
         if (numOfEntries < 3) {
             printf("Insufficient pairs of co-ordinates\n");
             return 0;
         }
         //notation sjk to mean the sum of x_i^j*y_i^k.
-        float s40 = getSx4(); //sum of x^4
-        float s30 = getSx3(); //sum of x^3
-        float s20 = getSx2(); //sum of x^2
-        float s10 = getSx();  //sum of x
-        float s00 = numOfEntries;
+        long double s40 = getSx4(); //sum of x^4
+        long double s30 = getSx3(); //sum of x^3
+        long double s20 = getSx2(); //sum of x^2
+        long double s10 = getSx();  //sum of x
+        long double s00 = numOfEntries;
         //sum of x^0 * y^0  ie 1 * number of entries
 
-        float s21 = getSx2y(); //sum of x^2*y
-        float s11 = getSxy();  //sum of x*y
-        float s01 = getSy();   //sum of y
+        long double s21 = getSx2y(); //sum of x^2*y
+        long double s11 = getSxy();  //sum of x*y
+        long double s01 = getSy();   //sum of y
 
         //b = Db/D
         return (s40*(s11 * s00 - s01 * s10) - 
@@ -79,22 +85,22 @@ public:
     }
 
     //returns the c term of the equation ax^2 + bx + c
-    float cTerm() {
+    long double cTerm() {
         if (numOfEntries < 3) {
             printf("Insufficient pairs of co-ordinates\n");
             return 0;
         }
         //notation sjk to mean the sum of x_i^j*y_i^k.
-        float s40 = getSx4(); //sum of x^4
-        float s30 = getSx3(); //sum of x^3
-        float s20 = getSx2(); //sum of x^2
-        float s10 = getSx();  //sum of x
-        float s00 = numOfEntries;
+        long double s40 = getSx4(); //sum of x^4
+        long double s30 = getSx3(); //sum of x^3
+        long double s20 = getSx2(); //sum of x^2
+        long double s10 = getSx();  //sum of x
+        long double s00 = numOfEntries;
         //sum of x^0 * y^0  ie 1 * number of entries
 
-        float s21 = getSx2y(); //sum of x^2*y
-        float s11 = getSxy();  //sum of x*y
-        float s01 = getSy();   //sum of y
+        long double s21 = getSx2y(); //sum of x^2*y
+        long double s11 = getSxy();  //sum of x*y
+        long double s01 = getSy();   //sum of y
 
         //c = Dc/D
         return (s40*(s20 * s01 - s10 * s11) - 
@@ -106,7 +112,7 @@ public:
                  s20 * (s30 * s10 - s20 * s20));
     }
     
-    float rSquare() { // get r-squared
+    long double rSquare() { // get r-squared
         if (numOfEntries < 3) {
             printf("Insufficient Entries.\n");
             return 0;
@@ -114,100 +120,119 @@ public:
         // 1 - (residual sum of squares / total sum of squares)
         return 1 - getSSerr() / getSStot();
     }
+
+    long double findPosZero() {
+        long double inSqrt=bTerm()*bTerm()-4*aTerm()*cTerm();
+        printf("inSqrt: %.3Le\n",inSqrt);
+        long double outSqrt=pow(inSqrt,.5);
+        printf("outSqrt: %.3Le\n",outSqrt);
+        long double posterm=-bTerm()+outSqrt;
+        posterm/=2;
+        posterm/=aTerm();
+        return posterm;
+    }
+    long double findNegZero() {
+        long double inSqrt=bTerm()*bTerm()-4*aTerm()*cTerm();
+        long double outSqrt=pow(inSqrt,.5);
+        long double negterm=-bTerm()-outSqrt;
+        negterm/=2;
+        negterm/=aTerm();
+        return negterm;
+    }
    
 
 private:
     int numOfEntries; 
-    float *pointpair; 
-    vector<pair<float,float> > pointArray; 
+    long double *pointpair; 
+    vector<pair<long double,long double> > pointArray; 
 
     //helper methods
-    float getSx() { //get sum of x
-        float Sx = 0;
+    long double getSx() { //get sum of x
+        long double Sx = 0;
         //FIX THAT!!!!
-        for (vector<pair<float, float> >::iterator it = pointArray.begin(); it != pointArray.end(); it++) {
+        for (vector<pair<long double, long double> >::iterator it = pointArray.begin(); it != pointArray.end(); it++) {
             Sx += (*it).first;
         }
         return Sx;
     }
 
-    double getSy() { //get sum of y
-        float Sy = 0;
-        for (vector<pair<float, float> >::iterator it = pointArray.begin(); it != pointArray.end(); it++) {
+    long double getSy() { //get sum of y
+        long double Sy = 0;
+        for (vector<pair<long double, long double> >::iterator it = pointArray.begin(); it != pointArray.end(); it++) {
             Sy += (*it).second;
         }
         return Sy;
     }
 
-    float getSx2() { //get sum of x^2
-        float Sx2 = 0;
-        for (vector<pair<float, float> >::iterator it = pointArray.begin(); it != pointArray.end(); it++) {
+    long double getSx2() { //get sum of x^2
+        long double Sx2 = 0;
+        for (vector<pair<long double, long double> >::iterator it = pointArray.begin(); it != pointArray.end(); it++) {
             Sx2 += pow((*it).first, 2); //sum of x^2
         }
         return Sx2;
     }
 
-    float getSx3() { //get sum of x^3
-        float Sx3 = 0;
-        for (vector<pair<float, float> >::iterator it = pointArray.begin(); it != pointArray.end(); it++) {
+    long double getSx3() { //get sum of x^3
+        long double Sx3 = 0;
+        for (vector<pair<long double, long double> >::iterator it = pointArray.begin(); it != pointArray.end(); it++) {
             Sx3 += pow((*it).first, 3); //sum of x^3
         }
         return Sx3;
     }
 
-    float getSx4() { //get sum of x^4
-        float Sx4 = 0;
-        for (vector<pair<float, float> >::iterator it = pointArray.begin(); it != pointArray.end(); it++) {
+    long double getSx4() { //get sum of x^4
+        long double Sx4 = 0;
+        for (vector<pair<long double, long double> >::iterator it = pointArray.begin(); it != pointArray.end(); it++) {
             Sx4 += pow((*it).first, 4); //sum of x^4
         }
         return Sx4;
     }
 
-    float getSxy() { //get sum of x*y
-        float Sxy = 0;
-        for (vector<pair<float, float> >::iterator it = pointArray.begin(); it != pointArray.end(); it++) {
+    long double getSxy() { //get sum of x*y
+        long double Sxy = 0;
+        for (vector<pair<long double, long double> >::iterator it = pointArray.begin(); it != pointArray.end(); it++) {
             Sxy += (*it).first * (*it).second; //sum of x*y
         }
         return Sxy;
     }
 
-    float getSx2y() { //get sum of x^2*y
-        float Sx2y = 0;
-        for (vector<pair<float, float> >::iterator it = pointArray.begin(); it != pointArray.end(); it++) {
+    long double getSx2y() { //get sum of x^2*y
+        long double Sx2y = 0;
+        for (vector<pair<long double, long double> >::iterator it = pointArray.begin(); it != pointArray.end(); it++) {
             Sx2y += pow((*it).first, 2) * (*it).second; //sum of x^2*y
         }
         return Sx2y;
     }
 
-    float getYMean() { //mean value of y
-        float y_tot = 0;
-        for (vector<pair<float, float> >::iterator it = pointArray.begin(); it != pointArray.end(); it++) {
+    long double getYMean() { //mean value of y
+        long double y_tot = 0;
+        for (vector<pair<long double, long double> >::iterator it = pointArray.begin(); it != pointArray.end(); it++) {
             y_tot += (*it).second; 
         }
         return y_tot/numOfEntries;
     }
 
-    float getSStot() { //total sum of squares
+    long double getSStot() { //total sum of squares
         //the sum of the squares of the differences between 
         //the measured y values and the mean y value
-        float ss_tot = 0;
-        for (vector<pair<float, float> >::iterator it = pointArray.begin(); it != pointArray.end(); it++) {
+        long double ss_tot = 0;
+        for (vector<pair<long double, long double> >::iterator it = pointArray.begin(); it != pointArray.end(); it++) {
             ss_tot += pow((*it).second - getYMean(), 2);
         }
         return ss_tot;
     }
 
-    float getSSerr() { //residual sum of squares
+    long double getSSerr() { //residual sum of squares
         //the sum of the squares of te difference between 
         //the measured y values and the values of y predicted by the equation
-        float ss_err = 0;
-        for (vector<pair<float, float> >::iterator it = pointArray.begin(); it != pointArray.end(); it++) {
+        long double ss_err = 0;
+        for (vector<pair<long double, long double> >::iterator it = pointArray.begin(); it != pointArray.end(); it++) {
             ss_err += pow((*it).second - getPredictedY((*it).first), 2);
         }
         return ss_err;
     }
 
-    float getPredictedY(float x) {
+    long double getPredictedY(long double x) {
         //returns value of y predicted by the equation for a given value of x
         return aTerm() * pow(x, 2) + bTerm() * x + cTerm();
     }
--- HW8/socket/testpolynomialfit.cpp
+++ HW8/socket/testpolynomialfit.cpp
@@ -1,13 +1,14 @@
 #include <stdio.h>
-#include "LstSquQuadRegr.cpp"
+#include "QuadReg.cpp"
   
 int main() {
-	float x[11] = {0,  1,  2,  3,  4,  5,  6,   7,   8,   9,   10};
-	float y[11] = {1,  6,  22, 36, 57, 86, 110, 162, 209, 262, 321};
+	float x[10] = {.2,  .4,  .6,  .8,  1,  1.2,  1.4,   1.6,   1.8,   2};
+	float y[10] = {690.1947, 690.1938, 690.1369, 690.08, 690.0231, 690.0231, 689.966, 689.909, 689.8525, 689.7957};
 	QuadReg QR;
 	for (int i = 0; i<11; i++) {
 		QR.AddPoints(x[i],y[i]);
 	}
-	printf("EQTN: %.3f*x^2 + %.3f*x + %.3f, R=%.3f\n",QR.aTerm(),QR.bTerm(),QR.cTerm(),QR.rSquare());
+	printf("EQTN: %.3Le*x^2 + %.3Le*x + %.3Le, R=%.3Le\n",QR.aTerm(),QR.bTerm(),QR.cTerm(),QR.rSquare());
+	printf("Pos: %.3Le, Neg: %.3Le\n",QR.findPosZero(),QR.findNegZero());
 	return 0;
 }
